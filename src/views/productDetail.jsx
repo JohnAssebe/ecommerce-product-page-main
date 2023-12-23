3ef6f37c267feb3ed440f8b059dfb3a6790f7fd3
John Assebe <johnassebe@gmail.com>
2023-12-24 01:52:54 +0300

    happy night :sparkles:

--- src/views/productDetail.jsx
+++ src/views/productDetail.jsx
@@ -33,7 +33,30 @@ const ProductDetail = () => {
             ))}
           </div>
         </div>
-        <div className="basis-1/2"></div>
+        <div className="flex items-center justify-start basis-1/2 font-kumbhSans">
+          <div className="flex flex-col items-start justify-center max-w-[400px] gap-4">
+            <h4 className="font-bold uppercase text-orange">sneaker company</h4>
+            <h2 className="text-4xl font-bold capitalize">
+              fall limited edition sneakers
+            </h2>
+            <p className="py-3 text-sm leading-5 text-darkgrayishblue">
+              These low-profile sneakers are your perfect casual wear companion.
+              Featuring a durabble rubber outer sole,they&apos;ll withstand
+              everything the weather can offer.
+            </p>
+            <div className="flex items-start justify-center gap-8 font-bold">
+              <div>
+                <h3 className="text-2xl">$125.00</h3>
+                <h5 className="text-lg line-through text-grayishblue">
+                  $250.00
+                </h5>
+              </div>
+              <div className="px-2 py-1 rounded-lg bg-paleOrange">
+                <p className="text-sm text-orange">50% off</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
