597b1bb66dfd0d25cf4112d401950772dd41a2fa
John Assebe <johnassebe@gmail.com>
2023-12-25 00:31:37 +0300

    update mobile responsiveness :sparkles:

--- src/views/productDetail.jsx
+++ src/views/productDetail.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react";
 import mainImages from "../utils/imageExporter";
+import minusIcon from "../assets/images/icon-minus.svg";
+import plusIcon from "../assets/images/icon-plus.svg";
+import cartIcon from "../assets/images/icon-cart.svg";
+import prevIcon from "../assets/images/icon-previous.svg";
+import nextIcon from "../assets/images/icon-next.svg";
+
 import { thumbnailImages } from "../utils/imageExporter";
 const ProductDetail = () => {
   const [imageIndex, setImageIndex] = useState(0);
+  const [slideCounter, setSlideCounter] = useState(0);
+  const [countProduct, setCountProduct] = useState(1);
+  const increaseCounter = () => setCountProduct((count) => (count += 1));
+  const decreaseCounter = () =>
+    countProduct > 0 && setCountProduct((count) => (count -= 1));
+
+  const nextSlide = () => setSlideCounter((count) => (count + 1) % 4);
+  const prevSlide = () =>
+    setSlideCounter((count) => (count - 1 > 0 ? count - 1 : count + 3) % 4);
+
+  console.log(slideCounter);
   return (
     <div className="w-full">
-      <section className="max-w-[1200px] mx-auto px-2 flex items-center justify-center mt-8">
-        <div className="flex flex-col flex-wrap items-center justify-center gap-6 px-3 basis-1/2">
+      <section className="max-w-[1200px] mx-auto px-2 flex flex-col md:flex-row items-center justify-center md:mt-16 gap-8 md:gap-0">
+        <div className="flex-col flex-wrap items-center justify-center hidden gap-6 px-3 md:flex md:basis-1/2">
           <div className="w-[24.5rem] h-96 rounded-2xl bg-orange">
             <img
               src={mainImages[imageIndex]}
@@ -33,7 +50,26 @@ const ProductDetail = () => {
             ))}
           </div>
         </div>
-        <div className="flex items-center justify-start basis-1/2 font-kumbhSans">
+        <div className="relative w-full rounded-lg md:hidden bg-orange">
+          <img
+            src={mainImages[slideCounter]}
+            alt="product"
+            className="object-cover w-full rounded-md h-80 sm:h-[400px]"
+          />
+          <div
+            className={`absolute flex items-center justify-center -translate-y-1/2 bg-white rounded-full w-10 h-10 left-4 top-1/2 `}
+            onClick={prevSlide}
+          >
+            <img src={prevIcon} />
+          </div>
+          <div
+            className="absolute flex items-center justify-center w-10 h-10 -translate-y-1/2 bg-white rounded-full right-4 top-1/2"
+            onClick={nextSlide}
+          >
+            <img src={nextIcon} />
+          </div>
+        </div>
+        <div className="flex items-center justify-start md:basis-1/2 font-kumbhSans">
           <div className="flex flex-col items-start justify-center max-w-[400px] gap-4">
             <h4 className="font-bold uppercase text-orange">sneaker company</h4>
             <h2 className="text-4xl font-bold capitalize">
@@ -53,6 +89,27 @@ const ProductDetail = () => {
               </div>
               <div className="px-2 py-1 rounded-lg bg-paleOrange">
                 <p className="text-sm text-orange">50% off</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-6 py-4 md:flex-row">
+              <div className="flex items-center justify-between px-4 py-2 rounded-lg w-36 bg-paleOrange">
+                <img
+                  src={minusIcon}
+                  onClick={decreaseCounter}
+                  className="cursor-pointer"
+                  alt="minus"
+                />
+                <p>{countProduct}</p>
+                <img
+                  src={plusIcon}
+                  onClick={increaseCounter}
+                  className="cursor-pointer"
+                  alt="plus"
+                />
+              </div>
+              <div className="flex items-center justify-center gap-3 px-6 py-3 text-sm font-bold text-white rounded-lg shadow-xl bg-orange">
+                <img src={cartIcon} alt="cart" className="w-4 h-4 fill-white" />
+                Add to cart
               </div>
             </div>
           </div>
